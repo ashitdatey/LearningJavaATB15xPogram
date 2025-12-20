6030c21fe851e156e257a9d5d2f86b86482b5903
Ashit Datey <dateyashit73@gmail.com>
2025-12-20 12:56:08 +0530

    Practice on Variables and Datatypes

--- src/ex_02_Variables_DataTypes/Lab003_No_Method.java
+++ src/ex_02_Variables_DataTypes/Lab003_No_Method.java
@@ -2,7 +2,6 @@ package ex_02_Variables_DataTypes;
 
 public class Lab003_No_Method {
     public static void main(String[] args) {
-
         System.out.println("Hi, I WILL be printed to the console.");
     }
 }
